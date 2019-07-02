3e973a84b912625ca94360456f4b51e1d6ae91d9
송승연 <wnzk413@gmail.com>
2019-07-02 15:43:24 +0900

    writeDay 구현

---
+++ app/src/main/java/sy/project2019/itshow/sasohan2019/Activity/WriteDay.java
@@ -0,0 +1,42 @@
+package sy.project2019.itshow.sasohan2019;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+import sy.project2019.itshow.sasohan2019.DB.DBHelper;
+
+public class WriteDay extends AppCompatActivity {
+
+    EditText title;
+    Button writeBtn;
+    DBHelper db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_day);
+
+        db = new DBHelper(getApplicationContext(), DBHelper.tableName, null,1);
+
+        writeBtn = findViewById(R.id.writeDayBtn);
+        title = findViewById(R.id.dayTitle);
+
+
+        final Calendar day = (Calendar) getIntent().getSerializableExtra("day");
+
+        writeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db.DateInsert(day.getTimeInMillis(), title.getText().toString());
+                Toast.makeText(getApplicationContext(), "일정이 등록되었습니다", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/sy/project2019/itshow/sasohan2019/Fragment/Fragment3.java
@@ -0,0 +1,60 @@
+package sy.project2019.itshow.sasohan2019;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
+import java.util.Calendar;
+
+public class Fragment3 extends Fragment {
+    View view;
+    TextView dayText;
+    ImageView writeBtn;
+    MaterialCalendarView calendarView;
+    Calendar currentDay;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_fragment3, container, false);
+
+        dayText = view.findViewById(R.id.today_rememberDay_frag3);
+        writeBtn = view.findViewById(R.id.writeDayBtn);
+        calendarView = view.findViewById(R.id.calendar);
+
+        calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                currentDay = date.getCalendar();
+            }
+        });
+
+        writeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(view.getContext(), WriteDay.class);
+                intent.putExtra("day", currentDay);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment3, container, false);
+
+
+    }
+
+}
